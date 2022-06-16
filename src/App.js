346c9b48afd53907b0bcf73a5e8f273bc9b1a6e6
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-06-16 14:48:22 +0600

    tring to learn useEffect hook.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // useEffect(() => {
-  // })
+
 
   const loginHandler = (email, password) => {
     setIsLoggedIn(true);
