89265a8b5c5c60816afad0838702b12fcfe86e82
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-06-16 15:35:45 +0600

    store login info to local storage and used useEffect hook to stay on login page after reload the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import LoginForm from "./components/LoginForm/LoginForm";
 import Home from "./components/Home/Home";
 
 function App() {
   const [isLogedIn, setIsLogedIn] = useState(false);
+  console.log("Component function evaluated by react");
+  useEffect(() => {
+    if (localStorage.getItem("isLogedIn") === "1") {
+      setIsLogedIn(true);
+    }
+    console.log("Side effect function runs!!");
+  }, []);
 
   const loginConfirmHandler = function (isLogIn, userInfo) {
-    console.log(userInfo);
+    // console.log(userInfo);
     setIsLogedIn(isLogIn);
+    localStorage.setItem("isLogedIn", "1");
   };
 
   const userLogOutHandler = function (isLogedOut) {
     setIsLogedIn(!isLogedOut);
+    localStorage.removeItem("isLogedIn");
   };
   return (
     <React.Fragment>
