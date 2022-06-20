61e75e07c2fe1fd304b2416c2a4371641b61d393
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-06-20 12:11:59 +0600

    learning react context and listen to context using useContext hook.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
@@ -6,14 +6,12 @@ import classes from "./Home.module.css";
 import AuthContext from "../../store/auth-context";
 
 const Home = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <AuthContext.Consumer>
-        {(ctx) => {
-          return <Button onClick={ctx.onLogout}>Logout</Button>;
-        }}
-      </AuthContext.Consumer>
+      <Button onClick={ctx.onLogout}>Logout</Button>;
     </Card>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = function (state, action) {
   if (action.type === "USER_INPUT") {
@@ -53,6 +54,8 @@ const Login = (props) => {
     };
   }, [passwordIsValid, emailIsValid]);
 
+  const ctx = useContext(AuthContext);
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
   };
@@ -69,7 +72,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
