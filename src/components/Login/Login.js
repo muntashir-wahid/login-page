b507bc0a5ce54b70c73e01fe0228fa61776ac91a
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-06-20 13:54:17 +0600

    manage the auth state and auth context into one file.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -72,7 +72,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,42 @@
-import React from "react";
-import { isCompositeComponent } from "react-dom/test-utils";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = function (props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = function (email, password) {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+    console.log(email, password);
+  };
+  const logoutHandler = function () {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
